84816b3fa73cb8536d9ae2d7fc9ab81da77f6df7
Erick Rodrigues da Silva <erickgamerh2o@gmail.com>
2026-04-13 17:50:47 -0300

    feat: enhance calculator functionality with operator handling and percentage calculation

--- js/script.js
+++ js/script.js
@@ -7,45 +7,106 @@ let operator = null;
 let restart = false;
 
 
-function updateResult(originClear = false){
+function updateResult(originClear = false) {
     result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
 }
 
-function addDigit(digit){
+function addDigit(digit) {
 
-    if(digit === "," && (currentNumber.includes(",") || !currentNumber)){
+    if (digit === "," && (currentNumber.includes(",") || !currentNumber)) {
         return;
     }
-    if(restart){
+    if (restart) {
         currentNumber = digit;
         restart = false
-    }else{
+    } else {
         currentNumber += digit;
     }
 
     updateResult();
 }
 
-function setOperator(newOperator){
+function setOperator(newOperator) {
 
-    if(currentNumber){
+    if (currentNumber) {
+
+        calculate();
+
         firstOperand = parseFloat(currentNumber.replace(",", "."));
         currentNumber = "";
     }
 
     operator = newOperator;
 }
+function calculate() {
+    if (operator === null || firstOperand === null) return;
+    let secondOperand = parseFloat(currentNumber.replace(",", "."));
+    let resultValue;
 
+    switch (operator) {
+
+        case "+": resultValue = firstOperand + secondOperand; break;
+        case "-": resultValue = firstOperand - secondOperand; break;
+        case "x": resultValue = firstOperand * secondOperand; break;
+        case "/": resultValue = firstOperand / secondOperand; break;
+        default: return;
+
+    }
+
+    if (resultValue.toString().split("."[1]?.length > 10)) {
+        currentNumber = parseFloat(resultValue.toFixed(10)).toString();
+    } else {
+        currentNumber = resultValue.toString();
+    }
+
+    operator = null;
+    firstOperand = null;
+    restart = true;
+    updateResult();
+
+
+}
+
+function clearCalculator() {
+    currentNumber = "";
+    firstOperand = null;
+    operator = null;
+    updateResult(true)
+}
+
+function setPercentage(){
+    let result = parseFloat(currentNumber) / 100;
+
+    if(["+", "-"].includes(operator)){
+        result = result * (firstOperand || 1);
+    }
+
+    if (result.toString().split(".")[1]?.length > 5){
+        result = result.toFixed(10).toString();
+    }
+
+    currentNumber = result.toString();
+    updateResult()
+}
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         const buttonText = button.innerText;
 
-        if(/^[0-9,]+$/.test(buttonText)){
+        if (/^[0-9,]+$/.test(buttonText)) {
 
             addDigit(buttonText)
-        }else if(["+", "-", "x", "/"].includes(buttonText)){
+        } else if (["+", "-", "x", "/"].includes(buttonText)) {
             setOperator(buttonText);
+        } else if (buttonText === "=") {
+            calculate();
+        } else if (buttonText === "C") {
+            clearCalculator();
+        } else if (buttonText === "±") {
+            currentNumber = (parseFloat(currentNumber || firstOperand) * -1).toString();
+            updateResult();
+        }else if(buttonText === "%"){
+            setPercentage();
         }
     })
 })
