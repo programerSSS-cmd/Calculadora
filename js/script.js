eb9446ef6c09d6e2ba645c358eac8d6c99d6fb5b
Erick Rodrigues da Silva <erickgamerh2o@gmail.com>
2026-04-13 16:27:35 -0300

    feat: implement basic calculator functionality with digit input and operator handling

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+
+function updateResult(originClear = false){
+    result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+function addDigit(digit){
+
+    if(digit === "," && (currentNumber.includes(",") || !currentNumber)){
+        return;
+    }
+    if(restart){
+        currentNumber = digit;
+        restart = false
+    }else{
+        currentNumber += digit;
+    }
+
+    updateResult();
+}
+
+function setOperator(newOperator){
+
+    if(currentNumber){
+        firstOperand = parseFloat(currentNumber.replace(",", "."));
+        currentNumber = "";
+    }
+
+    operator = newOperator;
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const buttonText = button.innerText;
+
+        if(/^[0-9,]+$/.test(buttonText)){
+
+            addDigit(buttonText)
+        }else if(["+", "-", "x", "/"].includes(buttonText)){
+            setOperator(buttonText);
+        }
+    })
+})
